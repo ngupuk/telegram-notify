d3b3d0a81fa88cbe57cdd967c773ae2d3be45915
Muhammad Wafa <wafax.4@gmail.com>
2020-12-07 23:41:21 +0700

    update github action url

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Sending message..."
 curl -s \
     --data-urlencode "chat_id=$2" \
     --data-urlencode "parse_mode=Markdown" \
-    --data-urlencode "text=[$GITHUB_REPOSITORY](https://github.com/$GITHUB_REPOSITORY/runs/$GITHUB_RUN_ID): 
+    --data-urlencode "text=[$GITHUB_REPOSITORY](https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA/checks): 
 
 $3" \
     https://api.telegram.org/bot$1/sendMessage > /dev/null
