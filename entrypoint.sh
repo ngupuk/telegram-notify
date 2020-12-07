6c2a89317c9170d73d49e3bb5e789a9fa908044d
Muhammad Wafa <wafax.4@gmail.com>
2020-12-07 23:05:12 +0700

    update message to sent

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ apk add curl --no-cache
 
 curl \
     --data-urlencode "chat_id=$2" \
-    --data-urlencode "text=$3" \
+    --data-urlencode "parse_mode=Markdown" \
+    --data-urlencode "text=[$GITHUB_REPOSITORY](https://github.com/$GITHUB_REPOSITORY/runs/$GITHUB_RUN_ID): \n$3" \
     https://api.telegram.org/bot$1/sendMessage
